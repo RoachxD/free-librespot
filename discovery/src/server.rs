05fdc7a7b9e733bc7cc4b98b9b68af0ef71102e4
Adrian Nicolae <nicolae.adrian.m@gmail.com>
2023-06-01 23:29:15 +0300

    More testing.

--- discovery/src/server.rs
+++ discovery/src/server.rs
@@ -101,7 +101,7 @@ impl RequestHandler {
             // TODO: bitmask but what are the flags?
             "supported_capabilities": 1,
             // undocumented but should still work
-            "accountReq": "PREMIUM",
+            "accountReq": "FREE",
             "activeUser": active_user,
             // others seen-in-the-wild:
             // - "deviceAPI_isGroup": False
